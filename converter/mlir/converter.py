0d076a69c9eb30557fc510cc930bd4d88fe117a3
notogawa <n.ohkawa@gmail.com>
2017-06-05 09:40:36 +0900

    mlir: link object detection (for LinearFunctio)

--- converter/mlir/converter.py
+++ converter/mlir/converter.py
@@ -113,7 +113,8 @@ def patch_for_functions():
     target_functions = []
     for k,v in (list(F.__dict__.items()) + list(M.__dict__.items()) +
                 [ ('BatchNormalizationFunction', F.normalization.batch_normalization.BatchNormalizationFunction),
-                  ('Convolution2DFunction', F.connection.convolution_2d.Convolution2DFunction)
+                  ('Convolution2DFunction', F.connection.convolution_2d.Convolution2DFunction),
+                  ('LinearFunction', F.connection.linear.LinearFunction)
                 ]):
         if inspect.isclass(v):
             target_functions.append(v)
@@ -179,6 +180,8 @@ class Chainer(object):
                     creator = out2link[id(candidate)]
                 else:
                     creator = candidate.creator
+                if isinstance(creator, F.connection.linear.LinearFunction):
+                    creator = creator.caller
                 if isinstance(creator, F.connection.convolution_2d.Convolution2DFunction):
                     creator = creator.caller
                 if isinstance(creator, F.normalization.batch_normalization.BatchNormalizationFunction):
@@ -264,7 +267,9 @@ class Chainer(object):
             def find_params(n, ps):
                 for p in ps:
                     if isinstance(p, str):
-                        if isinstance(n.__dict__[p], numpy.ndarray):
+                        if isinstance(n.__dict__[p], variable.Variable):
+                            yield p
+                        elif isinstance(n.__dict__[p], numpy.ndarray):
                             yield p
                         else:
                             yield str(n.__dict__[p])
