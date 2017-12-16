414535605e8ffc729a0bc11e1195f471d8f55c6b
notogawa <n.ohkawa@gmail.com>
2017-12-16 21:30:24 +0900

    chainer2mlir: ClippedReLU

--- chainer2mlir/chainer/mlir/functions/activation/__init__.py
+++ chainer2mlir/chainer/mlir/functions/activation/__init__.py
@@ -1,4 +1,5 @@
 import chainer.mlir.functions.activation.elu
+import chainer.mlir.functions.activation.clipped_relu
 import chainer.mlir.functions.activation.leaky_relu
 import chainer.mlir.functions.activation.relu
 import chainer.mlir.functions.activation.sigmoid
