bfc40448b43955a4e402dd4cdc1aadd898ea5be3
notogawa <n.ohkawa@gmail.com>
2018-05-22 09:43:37 +0900

    chainer2mlir: patch for bias

--- chainer2mlir/chainer/mlir/links/connection/__init__.py
+++ chainer2mlir/chainer/mlir/links/connection/__init__.py
@@ -2,3 +2,4 @@ import chainer.mlir.links.connection.convolution_2d
 import chainer.mlir.links.connection.depthwise_convolution_2d
 import chainer.mlir.links.connection.linear
 import chainer.mlir.links.connection.scale
+import chainer.mlir.links.connection.bias
---
+++ chainer2mlir/chainer/mlir/links/connection/bias.py
@@ -0,0 +1,14 @@
+from chainer.links import Bias
+from chainer.mlir.patch import encode_ndarray, patched_link_call
+
+Bias.__call__ = patched_link_call(Bias.__call__)
+
+def to_mlir_node(self):
+    return {
+        b'name': 'Bias',
+        b'params': {
+            b'axis': self.axis,
+            b'b': encode_ndarray(self.b.data),
+        }
+    }
+Bias.to_mlir_node = to_mlir_node
