5f1505a84496845a5e57271b6dc7e4cc81b1a5ce
notogawa <n.ohkawa@gmail.com>
2018-09-10 16:33:37 +0900

    chainer2mlir: Bilinear

--- chainer2mlir/chainer/mlir/functions/array/__init__.py
+++ chainer2mlir/chainer/mlir/functions/array/__init__.py
@@ -3,3 +3,4 @@ import chainer.mlir.functions.array.concat
 import chainer.mlir.functions.array.pad
 import chainer.mlir.functions.array.reshape
 import chainer.mlir.functions.array.transpose
+import chainer.mlir.functions.array.resize_images
---
+++ chainer2mlir/chainer/mlir/functions/array/resize_images.py
@@ -0,0 +1,16 @@
+from chainer.functions import ResizeImages
+from chainer.mlir.patch import patched_function_apply, patched_function_call
+
+if hasattr(ResizeImages, 'apply'):
+    ResizeImages.apply = patched_function_apply(ResizeImages.apply)
+else:
+    ResizeImages.__call__ = patched_function_call(ResizeImages.__call__)
+
+def to_mlir_node(self):
+    return {
+        b'name': 'Bilinear2D',
+        b'params': {
+            b'size': (self.out_H, self.out_W)
+        }
+    }
+ResizeImages.to_mlir_node = to_mlir_node
