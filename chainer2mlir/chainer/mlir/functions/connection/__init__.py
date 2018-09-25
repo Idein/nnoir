c6d36fecb49e38c28c29eb26eebad3ca3205d6f5
Fujii Yuya <koiking213@gmail.com>
2018-09-25 12:44:36 +0900

    chainer2mlir: add deconv2d

--- chainer2mlir/chainer/mlir/functions/connection/__init__.py
+++ chainer2mlir/chainer/mlir/functions/connection/__init__.py
@@ -1,3 +1,4 @@
 import chainer.mlir.functions.connection.convolution_2d
+import chainer.mlir.functions.connection.deconvolution_2d
 import chainer.mlir.functions.connection.depthwise_convolution_2d
 import chainer.mlir.functions.connection.linear
