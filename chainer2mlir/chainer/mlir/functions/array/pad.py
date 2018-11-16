dabb2d98e51653e0f62cd5eceeeff88f01554954
notogawa <n.ohkawa@gmail.com>
2018-11-16 11:02:03 +0900

    chainer2mlir: fix pad constant value

--- chainer2mlir/chainer/mlir/functions/array/pad.py
+++ chainer2mlir/chainer/mlir/functions/array/pad.py
@@ -10,11 +10,20 @@ else:
 def to_mlir_node(self):
     [var] = self.chainer_input_variables
     if self.mode == 'constant':
+        if type(self.keywords['constant_values']) == int:
+            value = float(self.keywords['constant_values'])
+        elif type(self.keywords['constant_values'][0]) == tuple:
+            value = float(self.keywords['constant_values'][0][0])
+            for (b,a) in self.keywords['constant_values']:
+                if b != value or a != value:
+                    raise Exception('unsupported pad value "{}"'.format(self.keywords['constant_values']))
+        else:
+            value = float(self.keywords['constant_values'][0])
         return {
             b'name': 'ConstantPadding',
             b'params': {
                 b'pads': _validate_lengths(var.data, self.pad_width),
-                b'value': float(self.keywords['constant_values'])
+                b'value': float(self.keywords['constant_values'][0])
             }
         }
     else:
