dcfaf2bda63e1d09a7153839a788b446519d4a67
Fujii Yuya <koiking213@gmail.com>
2018-12-12 18:15:24 +0900

    mlir: implement "run" for sigmoid/softmax

--- mlir/mlir/edges/softmax.py
+++ mlir/mlir/edges/softmax.py
@@ -1,8 +1,11 @@
 import chainer.functions as F
 from .edge import Edge
+import numpy as np
 
 class Softmax(Edge):
     def __init__(self, inputs, outputs, **params):
         necessary_params = {'axis'}
         optional_params = set()
         super(Softmax, self).__init__(inputs, outputs, params, necessary_params, optional_params)
+    def run(self, x):
+        return np.exp(x) / np.sum(np.exp(x), self.params['axis'], keepdims=True)
