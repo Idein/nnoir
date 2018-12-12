91c99774f115020ffb7f12adf05f328b15a85288
Fujii Yuya <koiking213@gmail.com>
2018-12-12 18:16:05 +0900

    mlir2chainer: add tests

---
+++ mlir2chainer/mlir2chainer/tests/test_softmax.py
@@ -0,0 +1,24 @@
+import mlir
+import chainer
+import mlir2chainer
+import numpy as np
+import six
+import msgpack
+import util
+
+def test_softmax():
+    inputs  = [mlir.Node('v0', 'float', (10,10))]
+    outputs = [mlir.Node('v2', 'float', (10,10))]
+    nodes = inputs + outputs
+    input_names = [ x.name for x in inputs ]
+    output_names = [ x.name for x in outputs ]
+    function = mlir.edges.Softmax(input_names, output_names, axis=1)
+    result = mlir.MLIR('Softmax', 'mlir2chainer_test', 0.1, input_names, output_names, nodes, [function])
+    result.dump('softmax.mlir')
+
+    x = np.random.randn(10,10)
+    ref = function.run(x)
+    m = mlir2chainer.ChainerNN('softmax.mlir')
+    with chainer.using_config('train', False):
+        y = m(x)
+        assert(np.all(abs(y-ref).data<util.epsilon))
