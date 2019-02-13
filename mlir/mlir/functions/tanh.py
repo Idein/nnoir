ed0b69fe67a2e16ba19ba6c9a263a1f5f67ffece
notogawa <n.ohkawa@gmail.com>
2019-02-13 23:59:42 +0900

    mlir: support Tanh

---
+++ mlir/mlir/functions/tanh.py
@@ -0,0 +1,12 @@
+import chainer.functions as F
+from .function import Function
+import numpy as np
+
+class Tanh(Function):
+    def __init__(self, inputs, outputs, **params):
+        required_params = set()
+        optional_params = set()
+        super(Tanh, self).__init__(inputs, outputs, params, required_params, optional_params)
+    def run(self, x):
+        R = x.copy()
+        return np.tanh(R)
