57b7715585e0cf005bf66c2f0ea32859eb145c15
notogawa <n.ohkawa@gmail.com>
2019-02-18 17:02:15 +0900

    mlir-onnx: PEP 396

--- mlir-onnx/mlir_onnx/__init__.py
+++ mlir-onnx/mlir_onnx/__init__.py
@@ -1,1 +1,4 @@
 from .onnx import ONNX
+from mlir_onnx import _version
+
+__version__ = _version.__version__
--- mlir-onnx/setup.py
+++ mlir-onnx/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
+import os
+
+exec(open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mlir_onnx', '_version.py')).read())
+
 setup(
     name="mlir-onnx",
-    version="1.0.0",
+    version=__version__,
     description='ONNX to MLIR Converter',
     author='Idein Inc.',
     author_email='n.ohkawa@idein.jp',
