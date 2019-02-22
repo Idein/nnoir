5fe55b36b8a21ab15ee8ec18af8e9fadb03c880f
notogawa <n.ohkawa@gmail.com>
2019-02-22 10:47:02 +0900

    nnoir-chainer: remove unused functions

--- nnoir-chainer/nnoir_chainer/functions/connection/linear.py
+++ nnoir-chainer/nnoir_chainer/functions/connection/linear.py
@@ -1,15 +1,7 @@
 from chainer.functions.connection.linear import LinearFunction
 from nnoir_chainer.patch import patched_function_apply, patched_function_call
-import nnoir.functions as NNOIR
 
 if hasattr(LinearFunction, 'apply'):
     LinearFunction.apply = patched_function_apply(LinearFunction.apply)
 else:
     LinearFunction.__call__ = patched_function_call(LinearFunction.__call__)
-
-
-def to_nnoir_node(self, inputs, outputs):
-    return NNOIR.LinearFunction(inputs, outputs)
-
-
-LinearFunction.to_nnoir_node = to_nnoir_node
