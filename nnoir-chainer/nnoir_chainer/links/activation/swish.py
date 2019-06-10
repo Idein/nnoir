e282e6f5d2a05a700f1719dc1381d7179ce212ca
notogawa <n.ohkawa@gmail.com>
2019-06-10 15:54:04 +0900

    nnoir-chainer: fix format

--- nnoir-chainer/nnoir_chainer/links/activation/swish.py
+++ nnoir-chainer/nnoir_chainer/links/activation/swish.py
@@ -4,7 +4,9 @@ import nnoir.functions as NNOIR
 
 Swish.__call__ = patched_link_call(Swish.__call__)
 
+
 def to_nnoir_node(self, inputs, outputs):
     return NNOIR.Swish([x.name for x in inputs], [x.name for x in outputs], beta=encode_ndarray(self.beta.data))
+
 
 Swish.to_nnoir_node = to_nnoir_node
