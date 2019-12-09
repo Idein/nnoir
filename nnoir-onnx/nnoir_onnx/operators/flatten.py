80c06d7e50bad1b4a95e7d79d8583b0d3102c8b3
Christian <guriido@christian18.04-XPS-15>
2019-12-09 10:12:19 +0900

    add support to Flatten

---
+++ nnoir-onnx/nnoir_onnx/operators/flatten.py
@@ -0,0 +1,27 @@
+from functools import reduce
+import numpy as np
+from nnoir.functions import *
+from .utils import *
+
+
+class OpFlatten(Op):
+
+    def __init__(self, node):
+        super(OpFlatten, self).__init__(node)
+
+        self.axis = 1
+        for attr in node.attribute:
+            if attr.name == 'axis':
+                self.axis = attr.i
+
+    def to_function(self, env, constants):
+        [x] = self.node.input
+        flattened_shape = (reduce(lambda k, n: k*n, env[x].shape[:self.axis]),
+                           reduce(lambda k, n: k*n, env[x].shape[self.axis:]))
+        return [
+            Reshape(
+                list(self.node.input),
+                list(self.node.output),
+                shape=flattened_shape
+            )
+        ]
