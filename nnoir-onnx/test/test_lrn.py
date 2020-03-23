3f0fb8e0990730101bf499d6819172931f582d37
Ryohei Tokuda <tokuda@idein.jp>
2020-03-23 21:17:19 +0900

    nnoir-onnx: LRN

---
+++ nnoir-onnx/test/test_lrn.py
@@ -0,0 +1,32 @@
+from util import Base
+from onnx import TensorProto
+from onnx.helper import make_node, make_graph, make_model, make_tensor_value_info, make_tensor, make_opsetid
+from onnx.numpy_helper import from_array
+import onnx
+import numpy as np
+
+from nose.tools import raises
+
+info = make_tensor_value_info
+
+
+def test_lrn_00():
+    '''
+    opser version >= 1
+
+    this test is not passed currently.
+    '''
+    class LRNTester(Base):
+        def create_onnx(self) -> onnx.ModelProto:
+            node = make_node("LRN", inputs=["v0"], outputs=["v1"],
+                             alpha=0.0002, beta=0.6, bias=0.8, size=3)
+            inputs = [info("v0", TensorProto.FLOAT, (5, 5, 5, 5))]
+            outputs = [info("v1", TensorProto.FLOAT, (5, 5, 5, 5))]
+            graph = make_graph([node], "add_graph", inputs, outputs)
+            model = make_model(graph)
+            return model
+
+    v0 = np.random.rand(5, 5, 5, 5).astype(np.float32)
+
+    outputs = ["v1"]
+    LRNTester({"v0": v0}, outputs).run()
