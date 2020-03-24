a421ced9549c22e58414d50d41c2096ddf0c5f31
Ryohei Tokuda <tokuda@idein.jp>
2020-03-24 16:44:44 +0900

    nnoir-onnx: append pads (opset version >= 11)

--- nnoir-onnx/nnoir_onnx/operators/pad.py
+++ nnoir-onnx/nnoir_onnx/operators/pad.py
@@ -11,24 +11,50 @@ class OpPad(Op):
         self.mode = b'constant'
         self.pads = None
         self.value = 0.0
-        for attr in node.attribute:
-            if attr.name == 'mode':
-                self.mode = attr.s
-            if attr.name == 'pads':
-                self.pads = attr.ints
-            if attr.name == 'value':
-                self.value = attr.f
 
+    def to_function(self, env, constants):
+        if self.opset_version >= 11:
+            # pads
+            if not self.node.input[1] in constants:
+                raise UnsupportedONNXOperation(self.node, 'pads must be "constant"')
+            self.pads = constants[self.node.input[1]]
+
+            # optional: constant_value
+            if len(self.node.input) >= 3:
+                if not self.node.input[2] in constants:
+                    raise UnsupportedONNXOperation(self.node, 'constant_value must be "constant"')
+
+                self.value = constants[self.node.input[2]]  # constant_value
+
+            for attr in self.node.attribute:
+                if attr.name == 'mode':
+                    self.mode = attr.s
+                else:
+                    raise UnsupportedONNXOperation(self.node, 'unknown attribute {}'.format(attr.s))
+
+            input_ = [self.node.input[0]]
+            pads = list(map(int, self.pads))  # In ONNX specification, the type of `pads` is int64
+        else:
+            # opset version >= 2
+            for attr in self.node.attribute:
+                if attr.name == 'mode':
+                    self.mode = attr.s
+                if attr.name == 'pads':
+                    self.pads = attr.ints
+                if attr.name == 'value':
+                    self.value = attr.f
+
+            input_ = list(self.node.input)
+            pads = self.pads
         if self.mode != b'constant':
             raise UnsupportedONNXOperation(self.node, 'mode must be "constant"')
 
-    def to_function(self, env, constants):
         n = len(self.pads) // 2
         return [
             ConstantPadding(
-                list(self.node.input),
+                input_,
                 list(self.node.output),
-                pads=list(zip(self.pads[:n], self.pads[n:])),
+                pads=list(zip(pads[:n], pads[n:])),
                 value=self.value,
             )
         ]
