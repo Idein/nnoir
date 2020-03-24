3dae2307200569fc6c01619d31985ce46877aaf8
Ryohei Tokuda <tokuda@idein.jp>
2020-03-24 14:44:01 +0900

    nnoir-onnx: Grouped conv

--- nnoir-onnx/test/test_conv.py
+++ nnoir-onnx/test/test_conv.py
@@ -26,3 +26,26 @@ def test_Conv_00():
 
     outputs = ["y"]
     ConvTester({"x": x}, outputs).run()
+
+
+def test_Conv_01():
+    x_shape = (1, 4, 4, 5)
+
+    class GroupedConvTester(Base):
+        def create_onnx(self) -> onnx.ModelProto:
+            node = make_node("Conv", inputs=["x", "W", "B"], outputs=["y"],
+                             pads=[1, 1, 1, 1], kernel_shape=[3, 3], group=2)
+            inputs = [info("x", TensorProto.FLOAT, x_shape)]
+            outputs = [info("y", TensorProto.FLOAT, (1, 4, 4, 5))]
+
+            W = from_array(np.random.rand(4, 2, 3, 3).astype(np.float32), "W")
+            B = from_array(np.random.rand(4).astype(np.float32), "B")
+
+            graph = make_graph([node], "add_graph", inputs, outputs, initializer=[W, B])
+            model = make_model(graph)
+            return model
+
+    x = np.random.rand(*x_shape).astype(np.float32)
+
+    outputs = ["y"]
+    GroupedConvTester({"x": x}, outputs).run()
