9193b362f5efe75693885051138ab02985635d83
Guriido <christian.hamelain@laposte.net>
2020-03-30 10:09:11 +0900

    remove support for opset 6 and less - support multidirectionnal broadcast

--- nnoir-onnx/nnoir_onnx/operators/mul.py
+++ nnoir-onnx/nnoir_onnx/operators/mul.py
@@ -7,57 +7,30 @@ class OpMul(Op):
     def __init__(self, node, *args):
         super().__init__(node, *args)
 
-        self._axis = None
-
-        for attr in node.attribute:
-            # opset version 1
-            if attr.name == 'consumed_inputs':
-                pass  # legacy attribute
-
-            # opset version <= 6
-            elif attr.name == 'broadcast':
-                # assume both inputs have the same shape if broadcast is disabled
-                pass
-            elif attr.name == 'axis':
-                self._axis = attr.i
-
-            else:
-                raise UnsupportedONNXOperation(self.node, 'unknown attribute {}'.format(attr.name))
-
     def to_function(self, env, constants):
         [a, b] = self.node.input
-
+        [y] = self.node.output
         def scale(v, w):
-            if self._axis is None:
-                # use unidirectional broadcasting rule
-                self._axis = env[v].ndim - env[w].ndim
-                if self._axis == 0 and not unidirectional_broadcastable(env[v].shape, env[w].shape):
-                    raise UnsupportedONNXOperation(
-                        self.node,
-                        'shape {} is not unidirectionnally broadcastable to shape {}'.format(env[w].shape, env[v].shape))
+            # use unidirectional broadcasting rule
+            axis = env[v].ndim - env[w].ndim
+            if axis == 0 and not unidirectional_broadcastable(env[v].shape, env[w].shape):
 
-            return [Scale([v], list(self.node.output), axis=self._axis, W=encode_ndarray(constants[w]), b=None)]
+                shape = env[y].shape
+                internal_node_val = env[v].reshape(shape)
+                internal_node = gen_unregisterd_node_name(env)
+                register_node(env, internal_node, internal_node_val)
+                return [
+                    Reshape([v], [internal_node], shape=shape),
+                    Scale([internal_node], list(self.node.output), axis=axis, W=encode_ndarray(constants[w]), b=None)
+                ]
+            else:
+                return [Scale([v], list(self.node.output), axis=axis, W=encode_ndarray(constants[w]), b=None)]
 
         if a in constants and b not in constants:
             return scale(b, a)
         elif a not in constants and b in constants:
             return scale(a, b)
         elif a not in constants and b not in constants:
-            if self._axis is not None:
-                a_shape = env[a].shape
-                b_shape = env[b].shape
-                shape_post_len = len(a_shape) - self._axis - len(b_shape)
-                shape = (1,)*self._axis + b_shape + (1,)*shape_post_len
-
-                internal_node_val = env[b].reshape(shape)
-                internal_node = gen_unregisterd_node_name(env)
-                register_node(env, internal_node, internal_node_val)
-
-                return [
-                    Reshape([b], [internal_node], shape=shape),
-                    Mul([a, internal_node], list(self.node.output))
-                ]
-            else:
-                return [Mul(list(self.node.input), list(self.node.output))]
+            return [Mul(list(self.node.input), list(self.node.output))]
         else:
             raise UnsupportedONNXOperation(self.node, 'bug! (unreachable here)')
--- nnoir-onnx/test/test_mul.py
+++ nnoir-onnx/test/test_mul.py
@@ -38,11 +38,10 @@ def test_mul_00():
     MulTester({"A": a, "B": b}, outputs).run()
 
 
-@raises(UnsupportedONNXOperation)
 def test_mul_01():
     '''
     opset version >= 7
-    with one constant, does not support multidirectional broadcasting
+    with one constant, support multidirectional broadcasting
     '''
 
     a_shape = (1, 1, 3, 4)
@@ -60,64 +59,6 @@ def test_mul_01():
             b_init = from_array(B, "B")
             graph = make_graph([node], "add_graph", inputs, outputs, initializer=[b_init])
             model = make_model(graph)
-            return model
-
-    a = np.random.rand(*a_shape).astype(np.float32)
-
-    outputs = ["C"]
-    MulTester({"A": a}, outputs).run()
-
-
-def test_mul_02():
-    '''
-    opset version <= 6
-
-    support axis attribute
-    '''
-
-    a_shape = (1, 2, 3, 4)
-    b_shape = (2, 3)
-    out_shape = (1, 2, 3, 4)
-
-    class MulTester(Base):
-        def create_onnx(self) -> onnx.ModelProto:
-            node = make_node("Mul", inputs=["A", "B"], outputs=["C"], axis=1, broadcast=1)
-            inputs = [info("A", TensorProto.FLOAT, a_shape), info("B", TensorProto.FLOAT, b_shape)]
-            outputs = [info("C", TensorProto.FLOAT, out_shape)]
-
-            graph = make_graph([node], "add_graph", inputs, outputs)
-            model = make_model(graph, opset_imports=[make_opsetid("", 6)])
-            return model
-
-    a = np.random.rand(*a_shape).astype(np.float32)
-    b = np.random.rand(*b_shape).astype(np.float32)
-
-    outputs = ["C"]
-    MulTester({"A": a, "B": b}, outputs).run()
-
-
-def test_mul_03():
-    '''
-    opset version <= 6
-
-    support axis attribute with one constant
-    '''
-
-    a_shape = (1, 2, 3, 4)
-    b_shape = (2, 3)
-    out_shape = (1, 2, 3, 4)
-
-    class MulTester(Base):
-        def create_onnx(self) -> onnx.ModelProto:
-            node = make_node("Mul", inputs=["A", "B"], outputs=["C"], axis=1, broadcast=1)
-            inputs = [info("A", TensorProto.FLOAT, a_shape)]
-            outputs = [info("C", TensorProto.FLOAT, out_shape)]
-
-            B = np.random.rand(*b_shape).astype(np.float32)
-
-            b_init = from_array(B, "B")
-            graph = make_graph([node], "add_graph", inputs, outputs, initializer=[b_init])
-            model = make_model(graph, opset_imports=[make_opsetid("", 1)])
             return model
 
     a = np.random.rand(*a_shape).astype(np.float32)
