1974405f792bd27e50070c1fe2c3e5f467576383
notogawa <n.ohkawa@gmail.com>
2020-04-20 16:06:59 +0900

    nnoir-onnx: add convertible MatMul test

--- nnoir-onnx/test/test_matmul.py
+++ nnoir-onnx/test/test_matmul.py
@@ -10,8 +10,37 @@ from nose.tools import raises
 info = make_tensor_value_info
 
 
-@raises(Exception)
 def test_matmul_00():
+    '''
+    opset version >= 9
+
+    Currently, this test will fail due to missing implementations
+    '''
+
+    shape = (3, 4)
+    w_shape = (4, 5)
+    out_shape = (3, 5)
+
+    class MatMulTester(Base):
+        def create_onnx(self) -> onnx.ModelProto:
+            node = make_node("MatMul", inputs=["x", "W"], outputs=["y"])
+            inputs = [info("x", TensorProto.FLOAT, shape)]
+            outputs = [info("y", TensorProto.FLOAT, out_shape)]
+
+            init_W = from_array(np.random.rand(*w_shape).astype(np.float32), "W")
+
+            graph = make_graph([node], "add_graph", inputs, outputs, initializer=[init_W])
+            model = make_model(graph)
+            return model
+
+    x = np.random.rand(*shape).astype(np.float32)
+
+    outputs = ["y"]
+    MatMulTester({"x": x}, outputs).run()
+
+
+@raises(Exception)
+def test_matmul_01():
     '''
     opset version >= 9
 
