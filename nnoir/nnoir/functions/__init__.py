06077376958a4254dad7ac624f8f2bc4f3af26f1
ishiy <y.ishihara.1993@gmail.com>
2020-07-16 13:37:31 +0900

    Update nnoir/functions/__init__.py

--- nnoir/nnoir/functions/__init__.py
+++ nnoir/nnoir/functions/__init__.py
@@ -17,6 +17,7 @@ from .leaky_relu import LeakyReLU
 from .linear import Linear
 from .local_response_normalization import LocalResponseNormalization
 from .lstm import LSTM
+from .matmul import MatMul
 from .max_pooling_2d import MaxPooling2D
 from .mul_constant import MulConstant
 from .mul import Mul
