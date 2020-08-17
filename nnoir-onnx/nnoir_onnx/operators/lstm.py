1d910ec398622f68294090558652c729eb0b16df
ishiy <y.ishihara.1993@gmail.com>
2020-08-17 12:12:30 +0900

    Support initial_h and initial_c

--- nnoir-onnx/nnoir_onnx/operators/lstm.py
+++ nnoir-onnx/nnoir_onnx/operators/lstm.py
@@ -1,6 +1,9 @@
 from .utils import *
 from nnoir.functions import *
 
+import numpy as np
+
+
 class OpLSTM(Op):
 
     def __init__(self, node, *args):
@@ -30,43 +33,125 @@ class OpLSTM(Op):
             if attr.name == 'input_forget':
                 self.input_forget = attr.i
 
-
     def to_function(self, env, constants):
-        print("LSTM.to_function start")
-        print(self.activation_alpha)
-        print(self.activation_beta)
-        print(self.activations)
-        print(self.clip)
-        print(self.direction)
-        print(self.hidden_size)
-        print(self.input_forget)
-        print("LSTM.to_function end")
+        num_of_gates = 4
+        num_of_peepholes = 3
+        num_directions = (env[self.node.input[1]].shape)[0]
+        seq_length, batch_size, input_size = env[self.node.input[0]].shape
+        hidden_size = self.hidden_size
 
-        if self.direction == "forward":
+        if num_directions == 1 and self.direction == "forward":
+            graph = []
+            init_h = np.zeros((num_directions, batch_size, hidden_size)).astype(np.float32)
+            init_c = np.zeros((num_directions, batch_size, hidden_size)).astype(np.float32)
+            ps = np.zeros((num_directions, num_of_peepholes*hidden_size)).astype(np.float32)
             li = len(self.node.input)
             if li == 3:
                 [x, W, R] = self.node.input
-                Ws = np.split(env[W], 4, axis=1)
-                Rs = np.split(env[R], 4, axis=1)
-                WBs = np.zeros(4)
-                RBs = np.zeros(4)
+                Ws = np.split(env[W][0], num_of_gates)
+                Rs = np.split(env[R][0], num_of_gates)
+                WBs = np.zeros((num_of_gates, hidden_size))
+                RBs = np.zeros((num_of_gates, hidden_size))
+                sequence_lens = np.repeat(seq_length, batch_size).astype(np.int32)
+                h0 = gen_new_node(env, value=init_h)
+                c0 = gen_new_node(env, value=init_c)
+                graph += [
+                    Constant([], [h0], value=init_h),
+                    Constant([], [c0], value=init_c),
+                ]
+                Ps = ps
             elif li == 4:
                 [x, W, R, B] = self.node.input
-                Ws = np.split(env[W], 4, axis=1)
-                Rs = np.split(env[R], 4, axis=1)
-                WB, RB = np.split(env[B], 2, axis=1)
-                WBs = np.split(WB, 4, axis=1)
-                RBs = np.split(WB, 4, axis=1)
+                Ws = np.split(env[W][0], num_of_gates)
+                Rs = np.split(env[R][0], num_of_gates)
+                WB, RB = np.split(env[B][0], 2)
+                WBs = np.split(WB, num_of_gates)
+                RBs = np.split(RB, num_of_gates)
+                sequence_lens = np.repeat(seq_length, batch_size).astype(np.int32)
+                h0 = gen_new_node(env, value=init_h)
+                c0 = gen_new_node(env, value=init_c)
+                graph += [
+                    Constant([], [h0], value=init_h),
+                    Constant([], [c0], value=init_c),
+                ]
+                Ps = ps
+            elif li == 5:
+                [x, W, R, B, seq_lens] = self.node.input
+                Ws = np.split(env[W][0], num_of_gates)
+                Rs = np.split(env[R][0], num_of_gates)
+                WB, RB = np.split(env[B][0], 2)
+                WBs = np.split(WB, num_of_gates)
+                RBs = np.split(RB, num_of_gates)
+                sequence_lens = env[seq_lens]
+                h0 = gen_new_node(env, value=init_h)
+                c0 = gen_new_node(env, value=init_c)
+                graph += [
+                    Constant([], [h0], value=init_h),
+                    Constant([], [c0], value=init_c),
+                ]
+                Ps = ps
+            elif li == 6:
+                [x, W, R, B, seq_lens, H0] = self.node.input
+                Ws = np.split(env[W][0], num_of_gates)
+                Rs = np.split(env[R][0], num_of_gates)
+                WB, RB = np.split(env[B][0], 2)
+                WBs = np.split(WB, num_of_gates)
+                RBs = np.split(RB, num_of_gates)
+                sequence_lens = env[seq_lens]
+                h0 = gen_new_node(env, value=env[H0])
+                c0 = gen_new_node(env, value=init_c)
+                graph += [
+                    Constant([], [h0], value=env[H0]),
+                    Constant([], [c0], value=init_c),
+                ]
+                Ps = ps
+            elif li == 7:
+                [x, W, R, B, seq_lens, H0, C0] = self.node.input
+                Ws = np.split(env[W][0], num_of_gates)
+                Rs = np.split(env[R][0], num_of_gates)
+                WB, RB = np.split(env[B][0], 2)
+                WBs = np.split(WB, num_of_gates)
+                RBs = np.split(RB, num_of_gates)
+                sequence_lens = env[seq_lens]
+                h0 = gen_new_node(env, value=env[H0])
+                c0 = gen_new_node(env, value=env[C0])
+                graph += [
+                    Constant([], [h0], value=env[H0]),
+                    Constant([], [c0], value=env[C0]),
+                ]
+                Ps = ps
+            elif li == 8:
+                [x, W, R, B, seq_lens, H0, C0, P] = self.node.input
+                Ws = np.split(env[W][0], num_of_gates)
+                Rs = np.split(env[R][0], num_of_gates)
+                WB, RB = np.split(env[B][0], 2)
+                WBs = np.split(WB, num_of_gates)
+                RBs = np.split(RB, num_of_gates)
+                sequence_lens = env[seq_lens]
+                h0 = gen_new_node(env, value=env[H0])
+                c0 = gen_new_node(env, value=env[C0])
+                graph += [
+                    Constant([], [h0], value=env[H0]),
+                    Constant([], [c0], value=env[C0]),
+                ]
+                Ps = np.split(env[P][0], num_of_peepholes)
+            else:
+                raise UnsupportedONNXOperation(self.node, 'too many inputs')
 
             lo = len(self.node.output)
             if lo == 1:
                 [y] = self.node.output
+                y_h = gen_new_node(env)
+                y_c = gen_new_node(env)
             elif lo == 2:
                 [y, y_h] = self.node.output
+                y_c = gen_new_node(env)
             elif lo == 3:
                 [y, y_h, y_c] = self.node.output
+            elif lo == 0:
+                return []
             else:
-                raise UnsupportedONNXOperation(self.node, 'too many output')
+                raise UnsupportedONNXOperation(self.node, 'too many outputs')
 
             # i = sigmoid(np.dot(x, W_i) + np.dot(h0, R_i) + WB_i + RB_i)
             # o = sigmoid(np.dot(x, W_o) + np.dot(h0, R_o) + WB_o + RB_o)
@@ -75,56 +160,61 @@ class OpLSTM(Op):
             # c1 = f*c0 + g*i
             # h1 = o*tanh(c1)
 
-            def gen_new_node(env):
-                n = gen_unregisterd_node_name(env)
-                register_node(env, n, None)
-                return n
+            def gemm(env, x, W, WB, res):
+                w = gen_new_node(env)
+                wb = gen_new_node(env)
+                t0 = gen_new_node(env)
+                graph = [
+                    Constant([], [w], value=W),
+                    Constant([], [wb], value=WB),
+                    MatMul([x, w], [t0]),
+                    Add([t0, wb], [res])
+                ]
+                return graph
 
-            W_i = gen_new_node(env)
-            W_o = gen_new_node(env)
-            W_c = gen_new_node(env)
+            def gate(env, x, h, W, R, WB, RB, f, res):
+                t0 = gen_new_node(env)
+                t1 = gen_new_node(env)
+                t2 = gen_new_node(env)
 
-            WB_i = gen_new_node(env)
-            WB_o = gen_new_node(env)
-            WB_c = gen_new_node(env)
+                graph = []
+                graph += gemm(env, x, W, WB, t0)
+                graph += gemm(env, h, R, RB, t1)
+                graph += [
+                    Add([t0, t1], [t2]),
+                    f([t2], [res]),
+                ]
 
+                return graph
 
-            i0 = gen_new_node(env)
-            i1 = gen_new_node(env)
-            i  = gen_new_node(env)
-
-            o0 = gen_new_node(env)
-            o1 = gen_new_node(env)
-            o  = gen_new_node(env)
-
-            g0 = gen_new_node(env)
-            g1 = gen_new_node(env)
-            g  = gen_new_node(env)
-
-            c1 = gen_new_node(env)
+            i = gen_new_node(env)
+            o = gen_new_node(env)
+            f = gen_new_node(env)
+            g = gen_new_node(env)
 
             t0 = gen_new_node(env)
+            t1 = gen_new_node(env)
+            t2 = gen_new_node(env)
 
-            return [
-                Constant([],[W_i],value=Ws[0].transpose((0,2,1))),
-                Constant([],[WB_i],value=WBs[0]),
-                MatMul([x, W_i], [i0]),
-                Add([i0, WB_i], [i1]),
-                Sigmoid([i1], [i]),
-                Constant([],[W_o],value=Ws[1].transpose((0,2,1))),
-                Constant([],[WB_o],value=WBs[0]),
-                MatMul([x, W_o], [o0]),
-                Add([o0, WB_o], [o1]),
-                Sigmoid([o1], [o]),
-                Constant([],[W_c],value=Ws[3].transpose((0,2,1))),
-                Constant([],[WB_c],value=WBs[0]),
-                MatMul([x, W_c], [g0]),
-                Add([g0, WB_c], [g1]),
-                Tanh([g1], [g]),
-                Mul([g, i], [c1]),
-                Tanh([c1], [t0]),
-                Mul([o, t0], [y_h]),
+            graph += gate(env, x, h0, Ws[0].transpose(), Rs[0].transpose(), WBs[0], RBs[0], Sigmoid, i)
+            graph += gate(env, x, h0, Ws[1].transpose(), Rs[1].transpose(), WBs[1], RBs[1], Sigmoid, o)
+            graph += gate(env, x, h0, Ws[2].transpose(), Rs[2].transpose(), WBs[2], RBs[2], Sigmoid, f)
+            graph += gate(env, x, h0, Ws[3].transpose(), Rs[3].transpose(), WBs[3], RBs[3], Tanh, g)
+            graph += [
+                Mul([f, c0], [t0]),
+                Mul([g, i], [t1]),
+                Add([t0, t1], [y_c]),
+                Tanh([y_c], [t2]),
+                Mul([o, t2], [y_h]),
                 AddConstant([y_h], [y], value=0.0)
             ]
+
+            return graph
         else:
             raise UnsupportedONNXOperation(self.node, 'direction is not forward')
+
+
+def gen_new_node(env, value=np.ones(1)):
+    n = gen_unregisterd_node_name(env)
+    register_node(env, n, value)
+    return n
