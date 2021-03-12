961e189dda8946d6fc43ee0907ed4486f9edbce2
Ryohei Tokuda <tokuda@idein.jp>
2021-03-12 19:25:41 +0900

    Close #102

--- nnoir-onnx/nnoir_onnx/operators/mat_mul.py
+++ nnoir-onnx/nnoir_onnx/operators/mat_mul.py
@@ -1,5 +1,14 @@
-from nnoir.functions import *
-from .utils import *
+import numpy as np
+
+from nnoir.functions import MatMul, linear, Constant
+from .utils import Op, gen_unregisterd_node_name, register_node
+
+
+def gen_value(env, arr):
+    name = gen_unregisterd_node_name(env)
+    register_node(env, name, arr)
+
+    return name
 
 
 class OpMatMul(Op):
@@ -9,7 +18,7 @@ class OpMatMul(Op):
 
     def to_function(self, env, constants):
         [x, W] = self.node.input
-        if W in constants:
+        if W in constants and len(constants[W].shape) == 2 and np.prod(env[x].shape[1:]) == constants[W].shape[1]:
             return [
                 Linear(
                     [x],
@@ -18,5 +27,12 @@ class OpMatMul(Op):
                     b=None
                 )
             ]
+        elif W in constants:
+            const_name = gen_value(env, constants[W])
+            nodes = [
+                Constant([], [const_name], value=constants[W]),
+                MatMul([x, const_name], list(self.node.output))
+            ]
+            return nodes
         else:
             return [MatMul(list(self.node.input), list(self.node.output))]
