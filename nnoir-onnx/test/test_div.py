ce527bc1e26071c0f2c3490661984ec88b014949
Ryohei Tokuda <tokuda@idein.jp>
2021-05-26 19:53:29 +0900

    Append tests

--- nnoir-onnx/test/test_div.py
+++ nnoir-onnx/test/test_div.py
@@ -2,6 +2,7 @@ import numpy as np
 import onnx
 from onnx import TensorProto
 from onnx.helper import make_graph, make_model, make_node, make_tensor_value_info
+from onnx.numpy_helper import from_array
 from util import Base
 
 info = make_tensor_value_info
@@ -86,3 +87,59 @@ def test_div_02():
 
     outputs = ["v2"]
     DivTester({"v0": v0, "v1": v1}, outputs).run()
+
+
+def test_div_const_00():
+    """
+    Test for constants
+    """
+
+    class DivTester(Base):
+        def __init__(self, inputs, outputs):
+            super().__init__(inputs, outputs)
+
+        def create_onnx(self) -> onnx.ModelProto:
+            node = make_node("Div", inputs=["v0", "const"], outputs=["v1"])
+            inputs = [
+                info("v0", TensorProto.FLOAT, (1, 3, 4, 5)),
+            ]
+            outputs = [info("v1", TensorProto.FLOAT, (1, 3, 4, 5))]
+
+            # constant which dimension is (1)
+            const = from_array(np.random.rand(1).astype(np.float32), "const")
+            graph = make_graph([node], "div_graph", inputs, outputs, initializer=[const])
+            model = make_model(graph)
+            return model
+
+    v0 = np.random.rand(1, 3, 4, 5).astype(np.float32)
+
+    outputs = ["v1"]
+    DivTester({"v0": v0}, outputs).run()
+
+
+def test_div_const_01():
+    """
+    Test for constants
+    """
+
+    class DivTester(Base):
+        def __init__(self, inputs, outputs):
+            super().__init__(inputs, outputs)
+
+        def create_onnx(self) -> onnx.ModelProto:
+            node = make_node("Div", inputs=["v0", "const"], outputs=["v1"])
+            inputs = [
+                info("v0", TensorProto.FLOAT, (1, 3, 4, 5)),
+            ]
+            outputs = [info("v1", TensorProto.FLOAT, (1, 3, 4, 5))]
+
+            # constant which dimension is none
+            const = from_array(np.array(2.0).astype(np.float32), "const")
+            graph = make_graph([node], "div_graph", inputs, outputs, initializer=[const])
+            model = make_model(graph)
+            return model
+
+    v0 = np.random.rand(1, 3, 4, 5).astype(np.float32)
+
+    outputs = ["v1"]
+    DivTester({"v0": v0}, outputs).run()
