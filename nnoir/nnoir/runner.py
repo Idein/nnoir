9575d3b558767b64bc5d9fc8ad82d65b0dc513da
ishiy <y.ishihara.1993@gmail.com>
2021-12-15 16:15:06 +0900

    Format

--- nnoir/nnoir/runner.py
+++ nnoir/nnoir/runner.py
@@ -1,7 +1,8 @@
 import argparse
-from nnoir import load, NNTC, TestValue
-import numpy as np
 import sys
+
+import numpy as np
+from nnoir import NNTC, TestValue, load
 
 
 def pack_values(ns, vs):
@@ -18,36 +19,12 @@ def generate_inputs(vs, seed, base, scale):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='NNTC generator')
-    parser.add_argument(dest='nnoir',
-                        type=str,
-                        metavar='NNOIR',
-                        help='input nnoir file')
-    parser.add_argument('-o',
-                        '--output',
-                        dest='output',
-                        type=str,
-                        required=True,
-                        metavar='PATH',
-                        help='output file path')
-    parser.add_argument('--seed',
-                        dest='seed',
-                        type=int,
-                        metavar='INT',
-                        help='seed for inputs generation',
-                        default=0)
-    parser.add_argument('--base',
-                        dest='base',
-                        type=float,
-                        metavar='FLOAT',
-                        help='modify randn',
-                        default=0.0)
-    parser.add_argument('--scale',
-                        dest='scale',
-                        type=float,
-                        metavar='FLOAT',
-                        help='modify randn',
-                        default=1.0)
+    parser = argparse.ArgumentParser(description="NNTC generator")
+    parser.add_argument(dest="nnoir", type=str, metavar="NNOIR", help="input nnoir file")
+    parser.add_argument("-o", "--output", dest="output", type=str, required=True, metavar="PATH", help="output file path")
+    parser.add_argument("--seed", dest="seed", type=int, metavar="INT", help="seed for inputs generation", default=0)
+    parser.add_argument("--base", dest="base", type=float, metavar="FLOAT", help="modify randn", default=0.0)
+    parser.add_argument("--scale", dest="scale", type=float, metavar="FLOAT", help="modify randn", default=1.0)
 
     args = parser.parse_args()
 
@@ -55,13 +32,10 @@ def main():
     if nnoir_model.name == b"main":
         sys.exit(f"Error: invalid model name ({nnoir_model.name})")
 
-    input_values = [[v for v in nnoir_model.values if v.name == n][0]
-                    for n in nnoir_model.inputs]
+    input_values = [[v for v in nnoir_model.values if v.name == n][0] for n in nnoir_model.inputs]
 
     inputs = generate_inputs(input_values, args.seed, args.base, args.scale)
     outputs = nnoir_model.run(*inputs)
 
-    tc = NNTC(nnoir_model.name, pack_values(nnoir_model.inputs, inputs),
-              pack_values(nnoir_model.outputs, outputs))
+    tc = NNTC(nnoir_model.name, pack_values(nnoir_model.inputs, inputs), pack_values(nnoir_model.outputs, outputs))
     tc.dump(args.output)
-
