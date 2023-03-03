e16fa83fdd92ea553eea4c03b3d741f4fa63dece
eguchi1904 <eguchi.1904@gmail.com>
2023-03-03 13:29:28 +0900

    remove redudant Constant node

--- nnoir-onnx/nnoir_onnx/operators/hard_swish.py
+++ nnoir-onnx/nnoir_onnx/operators/hard_swish.py
@@ -10,21 +10,15 @@ class OpHardSwish(Op):
     def to_function(self, env, constants):
         [x] = self.node.input
         t0 = gen_unregisterd_node_name(env)
-        register_node(env, t0, np.array(3.0, dtype=np.float32))
+        register_node(env, t0, env[x])
         t1 = gen_unregisterd_node_name(env)
         register_node(env, t1, env[x])
         t2 = gen_unregisterd_node_name(env)
         register_node(env, t2, env[x])
-        t3 = gen_unregisterd_node_name(env)
-        register_node(env, t3, env[x])
-        t4 = gen_unregisterd_node_name(env)
-        register_node(env, t4, np.array(6.0, dtype=np.float32))
 
         return [
-            Constant([], [t0], value=np.array(3.0, dtype=np.float32)),
-            Add([x, t0], [t1]),
-            ClippedReLU([t1], [t2], upper=6.0),
-            Mul([x, t2], [t3]),
-            Constant([], [t4], value=np.array(6.0, dtype=np.float32)),
-            Div([t3, t4], list(self.node.output)),
+            AddConstant([x], [t0], value=3.0),
+            ClippedReLU([t0], [t1], upper=6.0),
+            Mul([x, t1], [t2]),
+            MulConstant([t2], list(self.node.output), value=1 / 6),
         ]
