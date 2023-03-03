bb8d7b76548f19c07f9f916ce850e77a294e4010
eguchi1904 <eguchi.1904@gmail.com>
2023-03-03 12:10:30 +0900

    update readme

--- nnoir-onnx/nnoir_onnx/onnx.py
+++ nnoir-onnx/nnoir_onnx/onnx.py
@@ -323,7 +323,6 @@ Set the values with the `--fix_dimension` option."""
             if generator in known_generator:
                 continue
             if generator is not None:
-
                 function = self.op_for_node(generator).to_function(self.nodes, self.constant_nodes)
 
                 inputs = list(chain.from_iterable(map(lambda x: x.inputs, function)))
