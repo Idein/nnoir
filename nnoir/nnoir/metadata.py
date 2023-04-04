982ba2ccba2ad7fc69e60f5192f1f911a96d5340
eguchi1904 <eguchi.1904@gmail.com>
2023-04-04 16:24:13 +0900

    type metadata

--- nnoir/nnoir/metadata.py
+++ nnoir/nnoir/metadata.py
@@ -1,40 +1,41 @@
 import argparse
 
 from .load import load
+from .nnoir import NNOIR
 
 
-def read_name(nnoir) -> str:
+def read_name(nnoir: NNOIR) -> str:
     return nnoir.name.decode("utf-8")
 
 
-def read_description(nnoir) -> str:
+def read_description(nnoir: NNOIR) -> str:
     return nnoir.description.decode("utf-8")
 
 
-def read_generator_name(nnoir) -> str:
+def read_generator_name(nnoir: NNOIR) -> str:
     return nnoir.generator_name.decode("utf-8")
 
 
-def read_generator_version(nnoir) -> str:
+def read_generator_version(nnoir: NNOIR) -> str:
     return nnoir.generator_version.decode("utf-8")
 
 
-def write_name(nnoir, name: str):
+def write_name(nnoir: NNOIR, name: str) -> None:
     nnoir.name = name.encode(encoding="utf-8")
 
 
-def write_description(nnoir, description: str) -> str:
+def write_description(nnoir: NNOIR, description: str) -> None:
     nnoir.description = description.encode(encoding="utf-8")
 
 
-def print_metadata(nnoir):
+def print_metadata(nnoir: NNOIR) -> None:
     print(f"name = {read_name(nnoir)}")
     print(f"description = {read_description(nnoir)}")
     print(f"generator.name = {read_generator_name(nnoir)}")
     print(f"generator.version = {read_generator_version(nnoir)}")
 
 
-def nnoir_metadata():
+def nnoir_metadata() -> None:
     parser = argparse.ArgumentParser(description="read/write NNOIR meta data")
     parser.add_argument(dest="input", type=str, metavar="NNOIR", help="input(NNOIR) file path")
     parser.add_argument(
