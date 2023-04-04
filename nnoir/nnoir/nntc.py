384099509b771638baf9934593d6b2e69518f6c0
eguchi1904 <eguchi.1904@gmail.com>
2023-04-04 14:41:10 +0900

    type nntc

--- nnoir/nnoir/nntc.py
+++ nnoir/nnoir/nntc.py
@@ -2,65 +2,65 @@ import io
 
 import msgpack
 import numpy as np
+from numpy.typing import NDArray
+from typing import Any, Dict, List
 
+class TestValue:
+    def __init__(self, name: bytes, ndarray: NDArray[Any]):
+        self.name = name
+        self.ndarray = ndarray
+
+    def to_dict(self) -> Dict[bytes, Any]:
+        def encode_ndarray(obj: NDArray[Any]) -> bytes:
+            x = None
+            with io.BytesIO() as out:
+                np.save(out, obj.copy()) # type: ignore
+                x = out.getvalue()
+            return x
+
+        return {b"value_name": self.name, b"ndarray": encode_ndarray(self.ndarray)}
+
+class TestCase:
+    def __init__(self, inputs: List[TestValue], outputs: List[TestValue]):
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def to_dict(self) -> Dict[bytes, Any]:
+        return {
+            b"inputs": [i.to_dict() for i in self.inputs],
+            b"outputs": [o.to_dict() for o in self.outputs],
+        }
 
 class NNTC:
-    def __init__(self, model_name, inputs, outputs):
+    def __init__(self, model_name: bytes, inputs: List[TestValue], outputs: List[TestValue]):
         self.version = 0
         self.model = model_name
         self.test_case = TestCase(inputs, outputs)
 
-    def to_dict(self):
+    def to_dict(self) -> Dict[bytes, Any]:
         return {
             b"version": self.version,
             b"model": self.model,
             b"test_case": self.test_case.to_dict(),
         }
 
-    def pack(self):
-        return msgpack.packb(self.to_dict(), use_bin_type=False)
+    def pack(self) -> bytes:
+        return msgpack.packb(self.to_dict(), use_bin_type=False) # type: ignore
 
-    def dump(self, file_name):
+    def dump(self, file_name: str) -> None:
         result = self.pack()
         with open(file_name, "w") as f:
             f.buffer.write(result)
 
 
-class TestCase:
-    def __init__(self, inputs, outputs):
-        self.inputs = inputs
-        self.outputs = outputs
 
-    def to_dict(self):
-        return {
-            b"inputs": [i.to_dict() for i in self.inputs],
-            b"outputs": [o.to_dict() for o in self.outputs],
-        }
-
-
-class TestValue:
-    def __init__(self, name, ndarray):
-        self.name = name
-        self.ndarray = ndarray
-
-    def to_dict(self):
-        def encode_ndarray(obj):
-            x = None
-            with io.BytesIO() as out:
-                np.save(out, obj.copy())
-                x = out.getvalue()
-            return x
-
-        return {b"value_name": self.name, b"ndarray": encode_ndarray(self.ndarray)}
-
-
-def _load_test_value(v):
+def _load_test_value(v: Dict[bytes, Any]) -> TestValue:
     name = v[b"value_name"]
-    arr = np.load(io.BytesIO(v[b"ndarray"]))
+    arr = np.load(io.BytesIO(v[b"ndarray"])) # type: ignore
     return TestValue(name, arr)
 
 
-def load_nntc(file_name):
+def load_nntc(file_name: str) -> NNTC:
     with open(file_name, "rb") as f:
         nntc = msgpack.unpackb(f.read(), raw=True)
     version = nntc[b"version"]
