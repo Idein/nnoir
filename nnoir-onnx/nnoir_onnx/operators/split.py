34d74d6bfc7de5fba0cce2b9eb52d5ba64f27973
Guriido <christian.hamelain@laposte.net>
2023-09-28 08:27:21 +0900

    allow any split

--- nnoir-onnx/nnoir_onnx/operators/split.py
+++ nnoir-onnx/nnoir_onnx/operators/split.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List
 
 import numpy as np
 from nnoir.functions import Constant, MatMul, Transpose
@@ -6,13 +6,19 @@ from nnoir.functions import Constant, MatMul, Transpose
 from .utils import Op, UnsupportedONNXOperation, gen_unregisterd_node_name, register_node
 
 
-def create_half_split_matrices(k: int) -> Tuple[np.ndarray, np.ndarray]:
-    k_2 = k // 2
+def create_split_matrices(k: int, sizes: List[int]) -> List[np.ndarray]:
 
-    eye = np.eye(k_2, dtype="float32")
-    zero = np.zeros((k_2, k_2), dtype="float32")
+    split_matrices = []
+    acc = 0
+    for sub_k in sizes:
+        zero_before = np.zeros((acc, sub_k), dtype="float32")
+        eye = np.eye(sub_k, dtype="float32")
+        zero_after = np.zeros((k - sub_k - acc, sub_k), dtype="float32")
 
-    return (np.concatenate([eye, zero]), np.concatenate([zero, eye]))
+        split_matrices.append(np.concatenate([zero_before, eye, zero_after]))
+        acc += sub_k
+
+    return split_matrices
 
 
 def gen_value(env, arr):
@@ -35,22 +41,23 @@ class OpSplit(Op):
         super().__init__(node, *args)
 
     def to_function(self, env, constants):
-        if len(self.node.input) > 1:
-            raise UnsupportedONNXOperation(self.node, "the number of inputs must be 1.")
-        if len(self.node.output) > 2:
-            raise UnsupportedONNXOperation(self.node, "the number of outputs must be 2.")
-
         split_axis = 0
         for attr in self.node.attribute:
             if attr.name == "axis":
                 split_axis = attr.i
 
+        output_sizes_on_axis = []
+        for output in self.node.output:
+            output_sizes_on_axis.append(env[output].shape[split_axis])
+
         shape = env[self.node.input[0]].shape
         k = shape[split_axis]
-        if k % 2 != 0:
-            raise Exception("Cannot reshape on odd size, shape {}, axis {}".format(shape, split_axis))
+        assert k == sum(
+            output_sizes_on_axis
+        ), f"outputs are not a split of input on for axis {split_axis} of input shape {shape}: {output_sizes_on_axis}"
 
-        matrice_up, matrice_down = create_half_split_matrices(k)
+        matrices = create_split_matrices(k, output_sizes_on_axis)
+
         transpose_perm_0 = list(range(len(shape)))
         transpose_perm_0.append(transpose_perm_0.pop(split_axis))
         transpose_perm_1 = list(range(len(shape)))
@@ -77,30 +84,15 @@ class OpSplit(Op):
         trans_shape = transpose_shape(shape, transpose_perm_0)
         trans_out = gen_dummy_value(env, trans_shape)
 
-        linear_up_shape = linear_shape(trans_shape, matrice_up.shape)
-        linear_up_out = gen_dummy_value(env, linear_up_shape)
+        nodes = [Transpose(list(self.node.input), [trans_out], axes=transpose_perm_0)]
+        for i, mat in enumerate(matrices):
+            _linear_shape = linear_shape(trans_shape, mat.shape)
+            linear_out = gen_dummy_value(env, _linear_shape)
 
-        linear_down_shape = linear_shape(trans_shape, matrice_down.shape)
-        linear_down_out = gen_dummy_value(env, linear_down_shape)
-
-        up_const = gen_value(env, matrice_up)
-        up_const_node = Constant([], [up_const], value=matrice_up)
-        down_const = gen_value(env, matrice_up)
-        down_const_node = Constant([], [down_const], value=matrice_down)
-
-        transpose_node = Transpose(list(self.node.input), [trans_out], axes=transpose_perm_0)
-        linear_up_node = MatMul([trans_out, up_const], [linear_up_out])
-        linear_down_node = MatMul([trans_out, down_const], [linear_down_out])
-        transpose_up_node = Transpose([linear_up_out], [self.node.output[0]], axes=transpose_perm_1)
-        transpose_down_node = Transpose([linear_down_out], [self.node.output[1]], axes=transpose_perm_1)
-        nodes = [
-            up_const_node,
-            down_const_node,
-            transpose_node,
-            linear_up_node,
-            linear_down_node,
-            transpose_up_node,
-            transpose_down_node,
-        ]
+            _const = gen_value(env, mat)
+            _const_node = Constant([], [_const], value=mat)
+            linear_node = MatMul([trans_out, _const], [linear_out])
+            transpose_node = Transpose([linear_out], [self.node.output[i]], axes=transpose_perm_1)
+            nodes.extend([_const_node, linear_node, transpose_node])
 
         return nodes
--- nnoir-onnx/test/test_split.py
+++ nnoir-onnx/test/test_split.py
@@ -83,11 +83,37 @@ def test_split_default_axis():
     SplitTester({"v0": v0}, outputs).run()
 
 
-@pytest.mark.xfail()
 def test_split_specify_split():
     """
-    Specify second input (optional parameter).
-    Due to lack of implementation, the second input is not supported.
+    Specify split attribute (opset 11).
+    """
+
+    class SplitTester(Base):
+        def __init__(self, inputs, outputs):
+            super().__init__(inputs, outputs)
+
+        def create_onnx(self) -> onnx.ModelProto:
+            node = make_node("Split", inputs=["v0"], outputs=["v1", "v2", "v3"], axis=3, split=[2, 3, 5])
+            inputs = [
+                info("v0", TensorProto.FLOAT, (1, 3, 4, 10)),
+            ]
+            outputs = [
+                info("v1", TensorProto.FLOAT, (1, 3, 4, 2)),
+                info("v2", TensorProto.FLOAT, (1, 3, 4, 3)),
+                info("v3", TensorProto.FLOAT, (1, 3, 4, 5)),
+            ]
+
+            graph = make_graph([node], "add_graph", inputs, outputs)
+            return make_model(graph, opset_imports=[make_opsetid("", 11)])
+
+    v0 = np.random.rand(1, 3, 4, 10).astype(np.float32)
+    outputs = ["v1", "v2", "v3"]
+    SplitTester({"v0": v0}, outputs).run()
+
+
+def test_split_specify_split_13():
+    """
+    Specify split input (opset 13).
     """
 
     class SplitTester(Base):
@@ -107,11 +133,10 @@ def test_split_specify_split():
             ]
 
             graph = make_graph([node], "add_graph", inputs, outputs)
-            model = make_model(graph)
-            return model
+            return make_model(graph, opset_imports=[make_opsetid("", 13)])
 
     v0 = np.random.rand(1, 3, 4, 10).astype(np.float32)
     p0 = np.array([2, 3, 5]).astype(np.int64)
 
-    outputs = ["v1", "v2"]
+    outputs = ["v1", "v2", "v3"]
     SplitTester({"v0": v0, "p0": p0}, outputs).run()
