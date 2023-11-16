9a7de65b397c3d413ee5ebac066a95df357ae60d
ishiy <y.ishihara.1993@gmail.com>
2023-11-16 13:21:32 +0900

    Fix poetry2nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication overrides;
         pkgs = nixpkgs.legacyPackages.${system};
         py = pkgs.python39;
         customOverrides = self: super: {
@@ -38,18 +43,18 @@
             }
           );
         };
-        nnoir = pkgs.poetry2nix.mkPoetryApplication {
+        nnoir = mkPoetryApplication {
           projectDir = ./nnoir;
           python = py;
-          overrides = pkgs.poetry2nix.overrides.withDefaults (
+          overrides = overrides.withDefaults (
             customOverrides
           );
           preferWheels = true;
         };
-        nnoir-onnx = pkgs.poetry2nix.mkPoetryApplication {
+        nnoir-onnx = mkPoetryApplication {
           projectDir = ./nnoir-onnx;
           python = py;
-          overrides = pkgs.poetry2nix.overrides.withDefaults (
+          overrides = overrides.withDefaults (
             customOverrides
           );
           preferWheels = true;
