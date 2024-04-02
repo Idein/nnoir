b86fb5a672d4d174ca74ac1a36007a7b5f3a78ad
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2024-04-02 11:56:50 +0900

    Fix slice in nnoir.

--- nnoir/nnoir/functions/slice.py
+++ nnoir/nnoir/functions/slice.py
@@ -8,19 +8,17 @@ from .function import Function
 
 class Slice(Function):
     def __init__(self, inputs: List[bytes], outputs: List[bytes], **params: Any):
-        required_params = {"axes", "starts", "ends"}
+        required_params = {"starts", "ends", "axes"}
         optional_params: Set[str] = set()
         super(Slice, self).__init__(inputs, outputs, params, required_params, optional_params)
 
     def run(self, x):  # type: ignore
-        axes = self.params["axes"]
         starts = self.params["starts"]
         ends = self.params["ends"]
+        axes = self.params["axes"]
 
         indices = [np.s_[:]] * x.ndim
         for i in range(len(axes)):
             indices[axes[i]] = np.s_[starts[i]:ends[i]]
         R = x[tuple(indices)].copy()
         return R
-
-
