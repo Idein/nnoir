e185daf65f6fc32ae56309fb35db16ae757c8ff6
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2024-06-25 11:24:22 +0900

    Fix format.

--- nnoir-onnx/nnoir_onnx/operators/softmax.py
+++ nnoir-onnx/nnoir_onnx/operators/softmax.py
@@ -10,7 +10,7 @@ from .utils import *
 class OpSoftmax(Op):
     def __init__(self, node: onnx.NodeProto, *args: Any):
         super(OpSoftmax, self).__init__(node, *args)
-        
+
         self.axis = -1 if self.opset_version >= 13 else 1
         for attr in self.node.attribute:
             if attr.name == "axis":
@@ -20,5 +20,5 @@ class OpSoftmax(Op):
         [x] = self.node.input
         if self.axis < 0:
             self.axis = env[x].ndim + self.axis
-            
+
         return [Softmax(list(self.node.input), list(self.node.output), axis=self.axis)]
